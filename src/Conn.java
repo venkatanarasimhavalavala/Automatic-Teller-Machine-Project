746b4387e820a2c44ecf6b3bb2d1767adaf72dcd
VALAVALA VENKATA NARASIMHA <159681684+venkatanarasimhavalavala@users.noreply.github.com>
2025-05-22 11:19:32 +0530

    Update Conn.java

--- src/Conn.java
+++ src/Conn.java
@@ -1,1 +1,18 @@
+//For JDBC connection we have 5 steps
+//Register the driver, create connection, create statement, execute query,  close connection
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{ 
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Narsi@1117");
+            s=c.createStatement();
+        
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
 
